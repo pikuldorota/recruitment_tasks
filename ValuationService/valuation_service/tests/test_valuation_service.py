2e85edc5cab32fabfed82b8caee724bb7c562ccc
Dorota Pikul <pikuldorota@gmail.com>
2021-12-19 18:48:06 +0100

    Change logging to be more informative
    Add info and debug logging to the program and fix all the warnings, style mismatches and typos.

--- ValuationService/valuation_service/tests/test_valuation_service.py
+++ ValuationService/valuation_service/tests/test_valuation_service.py
@@ -11,7 +11,7 @@ class ArgumentsTestCase(unittest.TestCase):
         existing_paths = ['tests_files/currencies_empty_cells.csv', 'tests_files/data_empty_cells.csv',
                           'tests_files/empty_output.csv', 'tests_files/nonempty_output.csv',
                           'tests_files/valid_currencies.csv',
-                          'tests_files/valid_data.csv', 'tests_files/valid_matchings.csv']
+                          'tests_files/valid_data.csv', 'tests_files/valid_matching.csv']
         for path in existing_paths:
             assert check_if_existing_path(path) == path
 
@@ -26,18 +26,18 @@ class ArgumentsTestCase(unittest.TestCase):
         valid_data_paths = ['tests_files/data_empty_cells.csv', 'tests_files/valid_data.csv']
         for path in valid_data_paths:
             assert check_if_valid_data_file(path) == path
-        invalid_data_paths = ['tests_files/empty_output.csv', 'tests_files/valid_matchings.csv',
+        invalid_data_paths = ['tests_files/empty_output.csv', 'tests_files/valid_matching.csv',
                               'tests_files/valid_currencies.csv']
         for path in invalid_data_paths:
             with self.assertRaises(argparse.ArgumentTypeError) as context:
                 check_if_valid_data_file(path)
-            self.assertTrue('File "{}" is not of valid data format'.format(path) in  str(context.exception))
+            self.assertTrue('File "{}" is not of valid data format'.format(path) in str(context.exception))
 
     def test_check_if_valid_currencies_file(self):
         valid_currencies_paths = ['tests_files/currencies_empty_cells.csv', 'tests_files/valid_currencies.csv']
         for path in valid_currencies_paths:
             assert check_if_valid_currencies_file(path) == path
-        invalid_currencies_paths = ['tests_files/empty_output.csv', 'tests_files/valid_matchings.csv',
+        invalid_currencies_paths = ['tests_files/empty_output.csv', 'tests_files/valid_matching.csv',
                                     'tests_files/valid_data.csv']
         for path in invalid_currencies_paths:
             with self.assertRaises(argparse.ArgumentTypeError) as context:
@@ -45,7 +45,7 @@ class ArgumentsTestCase(unittest.TestCase):
             self.assertTrue('File "{}" is not of valid currency format'.format(path) in str(context.exception))
 
     def test_check_if_valid_matching_file(self):
-        valid_matching_paths = ['tests_files/valid_matchings.csv']
+        valid_matching_paths = ['tests_files/valid_matching.csv']
         for path in valid_matching_paths:
             assert check_if_valid_matching_file(path) == path
         invalid_matching_paths = ['tests_files/empty_output.csv', 'tests_files/currencies_empty_cells.csv',
@@ -53,7 +53,7 @@ class ArgumentsTestCase(unittest.TestCase):
         for path in invalid_matching_paths:
             with self.assertRaises(argparse.ArgumentTypeError) as context:
                 check_if_valid_matching_file(path)
-            self.assertTrue('File "{}" is not of valid matching format'.format(path) in  str(context.exception))
+            self.assertTrue('File "{}" is not of valid matching format'.format(path) in str(context.exception))
 
 
 class SavingFileTestCase(unittest.TestCase):
@@ -78,50 +78,51 @@ class SavingFileTestCase(unittest.TestCase):
         path = "tests_files/nonempty_output.csv"
         with self.assertRaises(FileExistsError) as context:
             save_results(output_data, path)
-        self.assertTrue('File "{}" already contains data. Choose different output file.'.format(path) in str(context.exception))
+        self.assertTrue('File "{}" already contains data. Choose different output file.'.format(path)
+                        in str(context.exception))
 
 
 class ChangingCSVTestCase(unittest.TestCase):
     @staticmethod
     def test_add_total_price():
-        data = pd.read_csv('tests_files/valid_data.csv')
-        add_total_price(data)
-        assert 'total_price' in data
+        input_data = pd.read_csv('tests_files/valid_data.csv')
+        add_total_price(input_data)
+        assert 'total_price' in input_data
         data_with_total_price = pd.read_csv('tests_files/data_with_total_price.csv')
-        assert data.equals(data_with_total_price)
+        assert input_data.equals(data_with_total_price)
 
     def test_add_total_price_empty_cells(self):
-        data = pd.read_csv('tests_files/data_empty_cells.csv')
-        add_total_price(data)
-        assert 'total_price' in data
+        input_data = pd.read_csv('tests_files/data_empty_cells.csv')
+        add_total_price(input_data)
+        assert 'total_price' in input_data
         data_with_total_price = pd.read_csv('tests_files/data_with_total_price_empty_cells.csv')
-        assert data.equals(data_with_total_price)
+        assert input_data.equals(data_with_total_price)
         with self.assertLogs('val') as cm:
             logging.getLogger('val').error('Empty cells found in data file. The record will be omitted.')
         self.assertEqual(cm.output, ['ERROR:val:Empty cells found in data file. The record will be omitted.'])
 
     @staticmethod
     def test_convert_currency_to_PLN():
-        data = pd.read_csv('tests_files/valid_data.csv')
+        input_data = pd.read_csv('tests_files/valid_data.csv')
         currency = pd.read_csv('tests_files/valid_currencies.csv')
-        convert_currency_to_PLN(data, currency)
+        convert_currency_to_pln(input_data, currency)
         converted_data = pd.read_csv('tests_files/valid_data_converted.csv')
-        assert data.equals(converted_data)
+        assert input_data.equals(converted_data)
 
     def test_convert_currency_to_PLN_no_currency(self):
-        data = pd.read_csv('tests_files/valid_data.csv')
+        input_data = pd.read_csv('tests_files/valid_data.csv')
         currency = pd.read_csv('tests_files/currencies_empty_cells.csv')
-        convert_currency_to_PLN(data, currency)
+        convert_currency_to_pln(input_data, currency)
         with self.assertLogs('root') as cm:
             logging.getLogger('root').error('Ratio for currency was not found. This matching will be omitted.')
         self.assertEqual(cm.output, ['ERROR:root:Ratio for currency was not found. This matching will be omitted.'])
 
     def test_convert_currency_to_PLN_empty_cells(self):
-        data = pd.read_csv('tests_files/data_empty_cells.csv')
+        input_data = pd.read_csv('tests_files/data_empty_cells.csv')
         currency = pd.read_csv('tests_files/valid_currencies.csv')
-        convert_currency_to_PLN(data, currency)
+        convert_currency_to_pln(input_data, currency)
         converted_data = pd.read_csv('tests_files/invalid_data_converted.csv')
-        assert data.equals(converted_data)
+        assert input_data.equals(converted_data)
         with self.assertLogs('root') as cm:
             logging.getLogger('root').error('Not all data has required info. Incomplete rows will be omitted.')
         self.assertEqual(cm.output, ['ERROR:root:Not all data has required info. Incomplete rows will be omitted.'])
@@ -145,35 +146,35 @@ class ChangingCSVTestCase(unittest.TestCase):
             [(1, 2000, 'GBP', 2, 1), (3, 200, 'PLN', 1, 2), (5, 30, 'GBP', 2, 1),
              (3, 300, 'PLN', 5, 2), (5, 500, 'GBP', 2, 1)],
             columns=['id', 'price', 'currency', 'quantity', 'matching_id'])
-        matching = pd.DataFrame([(1, 1), (2, 2)], columns=['matching_id', 'top_priced_count'])
+        matching_frame = pd.DataFrame([(1, 1), (2, 2)], columns=['matching_id', 'top_priced_count'])
         currency = pd.read_csv("tests_files/valid_currencies.csv")
-        data = get_required_data(valid_data, matching, currency)
+        input_data = get_required_data(valid_data, matching_frame, currency)
         expected_data = pd.DataFrame([(1, 4000, 843.33, 'GBP', 2), (2, 1700, 250.00, 'PLN', 0)],
                                      columns=['matching_id', 'total_price', 'avg_price', 'currency',
                                               'ignored_products_count'])
-        print(data)
+        print(input_data)
         print(expected_data)
-        assert data.equals(expected_data)
+        assert input_data.equals(expected_data)
 
-        matching = pd.DataFrame([(1, 30), (2, 2)], columns=['matching_id', 'top_priced_count'])
-        data = get_required_data(valid_data, matching, currency)
+        matching_frame = pd.DataFrame([(1, 30), (2, 2)], columns=['matching_id', 'top_priced_count'])
+        input_data = get_required_data(valid_data, matching_frame, currency)
         expected_data = pd.DataFrame([(1, 5060, 843.33, 'GBP', 0), (2, 1700, 250, 'PLN', 0)],
                                      columns=['matching_id', 'total_price', 'avg_price', 'currency',
                                               'ignored_products_count'])
-        print(data)
+        print(input_data)
         print(expected_data)
-        assert data.equals(expected_data)
+        assert input_data.equals(expected_data)
 
         with self.assertLogs('val') as cm:
             logging.getLogger('val').error('Too many elements required. Counting all elements with matching_id.')
         self.assertEqual(cm.output, ['ERROR:val:Too many elements required. Counting all elements with matching_id.'])
 
-        matching = pd.DataFrame([(np.nan, 30), (2, np.nan)], columns=['matching_id', 'top_priced_count'])
-        print(matching)
-        data = get_required_data(valid_data, matching, currency)
+        matching_frame = pd.DataFrame([(np.nan, 30), (2, np.nan)], columns=['matching_id', 'top_priced_count'])
+        print(matching_frame)
+        input_data = get_required_data(valid_data, matching_frame, currency)
         expected_data = pd.DataFrame([], columns=['matching_id', 'total_price', 'avg_price', 'currency',
                                                   'ignored_products_count'])
-        assert data.equals(expected_data)
+        assert input_data.equals(expected_data)
 
         with self.assertLogs('val') as cm:
             logging.getLogger('val').error('Matching misses data. Omitting this matching.')
--- ValuationService/valuation_service/valuation_service.py
+++ ValuationService/valuation_service/valuation_service.py
@@ -2,9 +2,18 @@
 import argparse
 import logging
 import os
+import sys
+
 import pandas as pd
 
 pd.options.mode.chained_assignment = None
+
+
+def set_up_logger(level):
+    file_handler = logging.FileHandler(filename='valuation_service.log')
+    stream_handler = logging.StreamHandler(sys.stdout)
+    logging.basicConfig(handlers=[file_handler, stream_handler], level=level)
+
 
 def check_if_existing_path(path):
     if not os.path.exists(path):
@@ -16,13 +25,13 @@ def check_if_valid_data_file(path):
     check_if_existing_path(path)
 
     try:
-        data = pd.read_csv(path)
+        input_data = pd.read_csv(path)
     except pd.errors.EmptyDataError:
         raise argparse.ArgumentTypeError('File "{}" is not of valid data format'.format(path)) from None
 
     columns = ['id', 'price', 'currency', 'quantity', 'matching_id']
     for column in columns:
-        if column not in data.columns:
+        if column not in input_data.columns:
             raise argparse.ArgumentTypeError('File "{}" is not of valid data format'.format(path))
     return path
 
@@ -45,12 +54,12 @@ def check_if_valid_matching_file(path):
     check_if_existing_path(path)
 
     try:
-        matching = pd.read_csv(path)
+        matching_data = pd.read_csv(path)
     except pd.errors.EmptyDataError:
         raise argparse.ArgumentTypeError('File "{}" is not of valid matching format'.format(path)) from None
     columns = ['matching_id', 'top_priced_count']
     for column in columns:
-        if column not in matching.columns:
+        if column not in matching_data.columns:
             raise argparse.ArgumentTypeError('File "{}" is not of valid matching format'.format(path))
     return path
 
@@ -78,34 +87,41 @@ def get_commandline_arguments():
     return args
 
 
-def add_total_price(data):
-    data['total_price'] = data.apply(lambda row: row['price'] * row['quantity'], axis=1)
+def add_total_price(input_data):
+    input_data['total_price'] = input_data.apply(lambda row: row['price'] * row['quantity'], axis=1)
 
 
-def convert_currency_to_PLN(data, currency):
+def convert_currency_to_pln(input_data, currency):
     currency_dict = {row['currency']: row['ratio'] for _, row in currency.iterrows()}
-    for curr in data['currency'].unique():
+    for curr in input_data['currency'].unique():
         if not pd.isna(curr) and (curr not in currency_dict or pd.isna(currency_dict[curr])):
             logging.error('Ratio for currency was not found. This matching will be omitted.')
             return False
-    data['price'] = data.apply(lambda row: row['price'] if pd.isna(row['currency']) else round(row['price'] / currency_dict[row['currency']], 2), axis=1)
-    data['currency'] = data.apply(lambda row: row['currency'] if pd.isna(row['price']) or pd.isna(row['currency']) else 'PLN', axis=1)
+
+    input_data['price'] = input_data.apply(
+        lambda row: row['price'] if pd.isna(row['currency']) else
+        round(row['price'] / currency_dict[row['currency']], 2), axis=1)
+
+    input_data['currency'] = input_data.apply(
+        lambda row: row['currency'] if pd.isna(row['price']) or pd.isna(row['currency']) else 'PLN', axis=1)
     return True
 
 
-def check_if_all_same_currency(data):
-    return len(data['currency'].unique()) == 1
+def check_if_all_same_currency(input_data):
+    return len(input_data['currency'].unique()) == 1
 
 
-def get_required_data(data, matchings, currency):
+def get_required_data(input_data, matching_frame, currency):
     def process_row(matching_id, top_priced_count):
         if pd.isna(matching_id) or pd.isna(top_priced_count):
             logging.error('Matching misses data. Omitting this matching.')
             return
-        matched_products = data.loc[data['matching_id'] == matching_id]
+        logging.debug("Counting result for matching with id {} and top priced count {}"
+                      .format(matching_id, top_priced_count))
+        matched_products = input_data.loc[input_data['matching_id'] == matching_id]
 
         if not check_if_all_same_currency(matched_products):
-            if not convert_currency_to_PLN(matched_products, currency):
+            if not convert_currency_to_pln(matched_products, currency):
                 return
         add_total_price(matched_products)
         matched_products = matched_products.sort_values(by=['total_price'], ascending=False)
@@ -115,23 +131,29 @@ def get_required_data(data, matchings, currency):
                     'total_price': top_products['total_price'].sum(),
                     'avg_price': round(matched_products['price'].mean(), 2),
                     'ignored_products_count': max(0, matched_products.shape[0] - top_priced_count)}
+        logging.debug("Counted result is: {}".format(row_dict))
         return row_dict
 
-    result = [process_row(matching_id, top) for matching_id, top in matchings.values]
-    result = pd.DataFrame([row for row in result if row is not None],
-                          columns=['matching_id', 'total_price', 'avg_price', 'currency', 'ignored_products_count'])
-    return result
+    result_rows = [process_row(matching_id, top) for matching_id, top in matching_frame.values]
+    result_rows = pd.DataFrame([row for row in result_rows if row is not None],
+                               columns=['matching_id', 'total_price', 'avg_price', 'currency',
+                                        'ignored_products_count'])
+    return result_rows
 
 
-def save_results(result, path="output.csv"):
+def save_results(calculated_data, path="output.csv"):
     if validate_that_output_file_is_empty_or_nonexistent(path):
-        result.to_csv(path, index=False)
+        calculated_data.to_csv(path, index=False)
 
 
 if __name__ == '__main__':
+    set_up_logger(logging.INFO)
     cmd_args = get_commandline_arguments()
+    logging.info("Reading the input csv files")
     data = pd.read_csv(cmd_args.data)
     matching = pd.read_csv(cmd_args.matching)
     currencies = pd.read_csv(cmd_args.currencies)
+    logging.info("Starting counting the valuation")
     result = get_required_data(data, matching, currencies)
+    logging.info("Saving the results")
     save_results(result)
